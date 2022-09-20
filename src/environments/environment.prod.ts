d22addcf063fb7ce9cb2d443f80ee2bbd9e49bda
Ramzi Melliti <r.melliti@protonmail.com>
2022-09-20 11:54:09 +0200

    build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  settings: {
+    api: 'https://api.beta.v8te.com',
+    livews: 'wss://api.beta.v8te.com/ws/elections/',
+    firebase: {
+      user: 'bogdandrb@gmail.com',
+      pass: 'v8tepass'
+    }
+  }
 };

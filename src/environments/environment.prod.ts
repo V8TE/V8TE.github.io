a560ca751385fbcfd5957d2ede5d285d9137684f
Ramzi Melliti <r.melliti@protonmail.com>
2022-10-11 15:01:06 +0200

    build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,26 @@
 export const environment = {
   production: true,
+  // settings: {
+  //   api: 'https://api.beta.v8te.com',
+  //   livews: 'wss://api.beta.v8te.com/ws/elections/',
+  //   firebase: {
+  //     user: 'bogdandrb@gmail.com',
+  //     pass: 'v8tepass'
+  //   }
+  // }
+
+  firebase: {
+    apiKey: "AIzaSyAY8vB8O5oG4WwHUL310u_JwqtC1X4iApU",
+    authDomain: "v8te-a9b50.firebaseapp.com",
+    databaseURL: "https://v8te-a9b50.firebaseio.com",
+    projectId: "v8te-a9b50",
+    storageBucket: "v8te-a9b50.appspot.com",
+    messagingSenderId: "418674463756",
+    appId: "1:418674463756:web:8f110be6b454dca9bb0a29",
+    measurementId: "G-P7SZ00CVHN"
+  },
   settings: {
-    api: 'https://api.beta.v8te.com',
-    livews: 'wss://api.beta.v8te.com/ws/elections/',
-    firebase: {
-      user: 'bogdandrb@gmail.com',
-      pass: 'v8tepass'
-    }
+    api: 'https://api.v8te.com',
+    livews: 'wss://api.v8te.com/ws/elections/',
   }
 };

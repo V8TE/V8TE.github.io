28a548b0b06a5965ebc06de9cc3218737ef6f180
Dan Radu-Bogdan <bogdandrb@gmail.com>
2026-03-03 08:22:51 +0100

    removed key

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,16 +3,5 @@ export const environment = {
   settings: {
     api: 'https://api.beta.v8te.com',
     livews: 'wss://api.beta.v8te.com/ws/elections/'
-  },
-
-  firebase: {
-    apiKey: "AIzaSyAY8vB8O5oG4WwHUL310u_JwqtC1X4iApU",
-    authDomain: "v8te-a9b50.firebaseapp.com",
-    databaseURL: "https://v8te-a9b50.firebaseio.com",
-    projectId: "v8te-a9b50",
-    storageBucket: "v8te-a9b50.appspot.com",
-    messagingSenderId: "418674463756",
-    appId: "1:418674463756:web:8f110be6b454dca9bb0a29",
-    measurementId: "G-P7SZ00CVHN"
   }
 };

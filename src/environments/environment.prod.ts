a847de305125a9bca05ead6b5bd5c2e2bd696bc7
Dan Radu-Bogdan <bogdandrb@gmail.com>
2026-03-18 14:32:37 +0100

    missing param

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   settings: {
     api: 'https://api.v8te.com',
-    apiNew: 'https://api.v8te.com'
+    apiNew: 'https://api-new.v8te.com'
   }
 };
